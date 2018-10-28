3b9e6c32202947ae4e6d61545d797fe7975111e3
Farida <faridah.absi@gmail.com>
2018-10-28 21:28:40 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,16 +1,26 @@
 // Code Keypad Component Here
 import React from 'react';
+<<<<<<< HEAD
 import ReactDOM from 'react-dom';
 
 
 export default class Keypad extends React.Component {
   keys = () =>{
+=======
+
+export default class Keypad extends React.Component {
+  tickle = () => {
+>>>>>>> f769f7fcf73e807ced5cc3ced19390d7876e0b22
     console.log('Entering password...');
   }
 
   render() {
     return (
+<<<<<<< HEAD
       <input type="password"  onKeyUp ={this.keys}/>
+=======
+      <input type="text" onkeyUp={this.tickle}></input>
+>>>>>>> f769f7fcf73e807ced5cc3ced19390d7876e0b22
     );
   }
 }
